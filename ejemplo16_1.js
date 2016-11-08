8163a13b7abb5ee6e8ccfecaa98332ac6a6da891
lilyE <ola.soi.lily@gmail.com>
2016-11-08 18:59:19 -0400

    Add files via upload

---
+++ ejemplo16_1.js
@@ -0,0 +1,13 @@
+//sentencia for
+function contadorA(cadena,letra){
+	for (var i = 0,contA=0;i<cadena.length;i++) {//permanencia
+		if (cadena[i]===letra) {//accion
+            contA++;
+		}
+}
+
+return contA;
+}
+var cad="mariamo";
+var letra="m";
+console.log("la cantidad de: "+letra+" , es:"+ contadorA(cad,letra));
